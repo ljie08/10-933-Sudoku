33709a82f54eb470d4467e460dce64b23d494c9b
ljie <ljie08@163.com>
2017-11-14 15:47:32 +0800

    add code

---
+++ MoxiLjieApp/网络/WebManager.h
@@ -0,0 +1,43 @@
+//
+//  WebManager.h
+//  MyWeather
+//
+//  Created by lijie on 2017/7/27.
+//  Copyright © 2017年 lijie. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+//请求成功回调block
+typedef void (^requestSuccessBlock)(NSDictionary *dic);
+
+//请求失败回调block
+typedef void (^requestFailureBlock)(NSError *error);
+
+//请求方法define
+typedef enum {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD
+} HTTPMethod;
+
+
+@interface WebManager : AFHTTPSessionManager
+
++ (instancetype)sharedManager;
+
+#pragma mark - Data
+//首页
+- (void)getHomeFMWithSuccess:(void(^)(NSDictionary *dic))success failure:(void(^)(NSString *errorStr))failure;
+
+
+#pragma mark - request
+- (void)requestWithMethod:(HTTPMethod)method
+                 WithUrl:(NSString *)url
+               WithParams:(NSDictionary*)params
+         WithSuccessBlock:(requestSuccessBlock)success
+          WithFailureBlock:(requestFailureBlock)failure;
+
+@end
